0b41178179e1358f8d6c837f7e93a48ffd25a4e3
paolaMedina <angie.paola.medina@correounivalle.edu.co>
2018-06-20 11:21:34 -0500

    boton add

--- app/src/main/java/com/example/jhonlp/proyectofinalapp/presentation/view/activity/MainActivity.java
+++ app/src/main/java/com/example/jhonlp/proyectofinalapp/presentation/view/activity/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.jhonlp.proyectofinalapp.presentation.view.activity;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
 
 import com.example.jhonlp.proyectofinalapp.R;
 import com.example.jhonlp.proyectofinalapp.presentation.presenter.AuthContract;
@@ -9,10 +13,32 @@ import com.example.jhonlp.proyectofinalapp.presentation.presenter.AuthPresenter;
 
 public class MainActivity extends AppCompatActivity implements AuthContract.View {
 
+    boolean click = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.menu_fab);
+        fab.setImageResource(R.drawable.icon_add);
+        fab.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                click = !click;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    Interpolator interpolador = AnimationUtils.loadInterpolator(getBaseContext(),
+                            android.R.interpolator.fast_out_slow_in);
+
+                    view.animate()
+                            .rotation(click ? 45f : 0)
+                            .setInterpolator(interpolador)
+                            .start();
+                }
+            }
+        });*/
 
         AuthPresenter mActionListener = new AuthPresenter(this);
 
